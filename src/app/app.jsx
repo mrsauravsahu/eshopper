20dfff57f5fabc2375f9f5db94d263b872996c21
Saurav Sahu <mrsauravsahu@outlook.com>
2021-03-03 16:44:47 +0530

    add placeholder components

---
+++ src/app/app.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Home } from '../components/home/home';
+import { Nav } from '../components/nav/nav';
+
+export const App = () => (
+  <>
+    <Nav />
+    <Home />
+  </>
+);
