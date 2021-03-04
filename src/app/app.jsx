f8b524bbf82ef1c235afb55616c507a194996396
Saurav Sahu <mrsauravsahu@outlook.com>
2021-03-04 23:19:28 +0530

    update state - add quantityAvaailable

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -16,6 +16,7 @@ export class App extends React.Component {
           unitPrice: 40,
           imageUrl: 'img/fruits/apple.png',
           quantityInBasket: 0,
+          quantityAvailable: 7,
         },
         {
           id: 2,
@@ -25,6 +26,7 @@ export class App extends React.Component {
           unitPrice: 80,
           imageUrl: 'img/fruits/mango.png',
           quantityInBasket: 0,
+          quantityAvailable: 4,
         },
         {
           id: 3,
@@ -34,6 +36,7 @@ export class App extends React.Component {
           unitPrice: 80,
           imageUrl: 'img/fruits/strawberry.png',
           quantityInBasket: 0,
+          quantityAvailable: 18,
         },
         {
           id: 4,
@@ -43,6 +46,7 @@ export class App extends React.Component {
           unitPrice: 80,
           imageUrl: 'img/fruits/peach.png',
           quantityInBasket: 0,
+          quantityAvailable: 10,
         },
         {
           id: 5,
@@ -52,6 +56,7 @@ export class App extends React.Component {
           unitPrice: 80,
           imageUrl: 'img/fruits/grape.png',
           quantityInBasket: 0,
+          quantityAvailable: 8,
         },
         {
           id: 6,
@@ -61,6 +66,7 @@ export class App extends React.Component {
           unitPrice: 80,
           imageUrl: 'img/fruits/pineapple.png',
           quantityInBasket: 0,
+          quantityAvailable: 4,
         },
         {
           id: 7,
@@ -70,6 +76,7 @@ export class App extends React.Component {
           unitPrice: 80,
           imageUrl: 'img/fruits/banana.png',
           quantityInBasket: 0,
+          quantityAvailable: 2,
         },
         {
           id: 8,
@@ -79,6 +86,7 @@ export class App extends React.Component {
           unitPrice: 80,
           imageUrl: 'img/fruits/cherry.png',
           quantityInBasket: 0,
+          quantityAvailable: 11,
         },
         {
           id: 9,
@@ -88,6 +96,7 @@ export class App extends React.Component {
           unitPrice: 80,
           imageUrl: 'img/fruits/orange.png',
           quantityInBasket: 0,
+          quantityAvailable: 3,
         },
       ],
     };
