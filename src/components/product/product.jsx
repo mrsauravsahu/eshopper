1ce43eecb9b994af563b8cdc614d7fa05cc3dc09
Saurav Sahu <mrsauravsahu@outlook.com>
2021-03-04 22:29:02 +0530

    add proptypes for Product component

--- src/components/product/product.jsx
+++ src/components/product/product.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 import styles from './product.module.css';
+
+export const ProductPropType = {
+  name: PropTypes.string.isRequired,
+  manufacturer: PropTypes.string.isRequired,
+  imageUrl: PropTypes.string.isRequired,
+  unitQuantity: PropTypes.number.isRequired,
+};
 
 export const Product = ({ product }) => (
   <div className={styles.container}>
     <img src={product.imageUrl} alt={`Purchase ${product.name}`} />
-    <div className={styles.manufacturer}>{product.manufacturer}</div>
+    <div clsssName={styles.manufacturer}>{product.manufacturer}</div>
     <div className={styles.name}>{product.name}</div>
     <div>{product.unitQuantity}</div>
   </div>
 );
+
+Product.propTypes = {
+  product: PropTypes.shape(ProductPropType).isRequired,
+};
