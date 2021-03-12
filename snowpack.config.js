bdef11ac3391936af7610325f7dd9067c3574543
Saurav Sahu <mrsauravsahu@outlook.com>
2021-03-12 08:44:17 +0530

    add api proxy

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,3 +1,8 @@
+// snowpack.config.js
+const httpProxy = require('http-proxy');
+
+const proxy = httpProxy.createServer({ target: 'http://localhost:5000' });
+
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
   mount: {
@@ -9,8 +14,15 @@ module.exports = {
     /* ... */
   ],
   routes: [
-    /* Enable an SPA Fallback in development: */
-    // {"match": "routes", "src": ".*", "dest": "/index.html"},
+    {
+      src: '/api/.*',
+      dest: (req, res) => {
+        // remove /api prefix (optional)
+        req.url = req.url.replace(/^\/api/, '');
+
+        proxy.web(req, res);
+      },
+    },
   ],
   optimize: {
     /* Example: Bundle your final build: */
